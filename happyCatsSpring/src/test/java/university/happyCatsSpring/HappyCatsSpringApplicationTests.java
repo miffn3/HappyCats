d5a4a985494278f7f6c8ca2d12e1aef0ae6c7a27
miffn3 <miffn3@yandex.ru>
2020-05-08 17:39:32 +0400

    feat: init spring app

---
+++ happyCatsSpring/src/test/java/university/happyCatsSpring/HappyCatsSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package university.happyCatsSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappyCatsSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
