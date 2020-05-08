f81f3a793f43eedf08486ae4edc85f84118a582a
miffn3 <miffn3@yandex.ru>
2020-05-08 17:39:32 +0400

    feat: init spring app

---
+++ happyCatsSpring/src/main/java/university/happyCatsSpring/HappyCatsSpringApplication.java
@@ -0,0 +1,13 @@
+package university.happyCatsSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyCatsSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HappyCatsSpringApplication.class, args);
+	}
+
+}
