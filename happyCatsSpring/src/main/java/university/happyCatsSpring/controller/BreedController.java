f6de7851f4ce9c3f721f7ae584d7e1c6ba4a1b46
miffn3 <miffn3@yandex.ru>
2020-05-17 02:48:39 +0400

    feat: get breed, news, disease by id

--- happyCatsSpring/src/main/java/university/happyCatsSpring/controller/BreedController.java
+++ happyCatsSpring/src/main/java/university/happyCatsSpring/controller/BreedController.java
@@ -3,12 +3,14 @@ package university.happyCatsSpring.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import university.happyCatsSpring.entity.Breed;
 import university.happyCatsSpring.service.iface.BreedService;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/breeds")
@@ -25,5 +27,20 @@ public class BreedController {
     @GetMapping(value = "/allbreeds")
     public ResponseEntity<List<Breed>> listBreed() {
         return ResponseEntity.ok(breedService.findAll());
+    }
+
+    @GetMapping(value = "/id/{breedid}")
+    public ResponseEntity<Breed> getBreedById(@PathVariable("breedid") String breedId)
+    {
+        Long id = Long.parseLong(breedId);
+
+        Optional<Breed> breedOptional = breedService.findById(id);
+
+        if (breedOptional.isPresent()) {
+            Breed breed = breedOptional.get();
+            return ResponseEntity.ok(breed);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
--- happyCatsSpring/src/main/java/university/happyCatsSpring/service/iface/DiseaseService.java
+++ happyCatsSpring/src/main/java/university/happyCatsSpring/service/iface/DiseaseService.java
@@ -4,8 +4,10 @@ import org.springframework.stereotype.Service;
 import university.happyCatsSpring.entity.Disease;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public interface DiseaseService {
     List<Disease> findAll();
+    Optional<Disease> findById(Long id);
 }
